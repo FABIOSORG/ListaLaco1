29163ef661bf6e7c37f714ec5e65b5def910bee2
Fabio Eduardo Sorg <fabiosorg1980@gmail.com>
2018-11-13 20:39:11 -0200

    Correção da classe referente à aplicação de operações aritméticas

--- src/exercicios/CalculoQuatroOperacoes.java
+++ src/exercicios/CalculoQuatroOperacoes.java
@@ -15,35 +15,34 @@ public class CalculoQuatroOperacoes {
     public static void main(String[] args) {
  
    
-int opc = 0, opc1 = 0, opc2 = 0, opc3 = 0, opc4 = 0;
+int opc = 0;
 double valor1, valor2, resultado;
 while (opc!=5){
     
-   opc= Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção:\n1- somar\n2- subtrair\n3- Multiplicar\n4- Dividir\n5- Sair"));
+   opc = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção:\n 1- somar\n 2- subtrair\n 3- Multiplicar\n 4- Dividir\n 5- Sair", JOptionPane.INFORMATION_MESSAGE));
+   if(opc==5){
+      
+      System.exit(0); 
+     }
+      
+   valor1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor: "));
+   valor2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor: "));
    
-   if(opc1==1){
-      valor1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor: "));
-      valor2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor: "));
+   if(opc==1){
       resultado = valor1+valor2;
-      JOptionPane.showMessageDialog(null,"A soma é igual a: "+resultado+"RESULTADO SOMA");
+      JOptionPane.showMessageDialog(null,"A soma é igual a: "+resultado+" RESULTADO SOMA ");
              }
-   if(opc2==2){
-      valor1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor: "));
-      valor2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor: "));
-      resultado= valor2-valor1;
-      JOptionPane.showMessageDialog(null,"A subtração é igual a: "+resultado+"RESULTADO SUBTRAÇÃO");
+   if(opc==2){
+      resultado= valor1-valor2;
+      JOptionPane.showMessageDialog(null,"A subtração é igual a: "+resultado+" RESULTADO SUBTRAÇÃO ");
      }
-   if(opc3==3){
-      valor1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor: "));
-      valor2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor: "));
+   if(opc==3){
       resultado= valor1*valor2;
-      JOptionPane.showMessageDialog(null,"A multiplicação é igual a: "+resultado+"RESULTADO MULTIPLICAÇÃO");
+      JOptionPane.showMessageDialog(null,"A multiplicação é igual a: "+resultado+" RESULTADO MULTIPLICAÇÃO ");
      }
-   if(opc4==4){
-      valor1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor: "));
-      valor2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor: "));
+   if(opc==4){
       resultado = valor1/valor2;
-      JOptionPane.showMessageDialog(null,"A divisão é igual a: "+resultado+"RESULTADO DIVISÃO");
+      JOptionPane.showMessageDialog(null,"A divisão é igual a: "+resultado+" RESULTADO DIVISÃO ");
      }
    
 }    
